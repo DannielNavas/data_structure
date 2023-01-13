96daf2fa9207ce18376ec4890881bde69291b194
Danniel Navas <dannielnavas@gmail.com>
2023-01-13 06:59:08 -0500

    ✨ replicate array methods

--- array.js
+++ array.js
@@ -22,7 +22,25 @@ class MyArray {
     this.length++;
     return this.data;
   }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+  delete(index) {
+    const item = this.data[index];
+    this.shiftIndex(index);
+    return item;
+  }
+  shiftIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
 
+  }
 }
 
 const myArray = new MyArray();
@@ -31,4 +49,12 @@ console.log(myArray.push("Daniel"));
 
 console.log(myArray.push("John"));
 
+console.log(myArray.push("Jane"));
+
 console.log(myArray.get(1));
+
+console.log(myArray.pop());
+
+console.log(myArray.delete(0));
+
+console.log(myArray);
