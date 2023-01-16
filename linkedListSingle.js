8c85226439ff986f51914147e77498de675cca99
Danniel Navas <dannielnavas@gmail.com>
2023-01-16 07:08:04 -0500

    ✨  added method singly linked list

--- linkedListSingle.js
+++ linkedListSingle.js
@@ -44,6 +44,34 @@ class MySingleListLinked {
     this.length++;
     return this;
   }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  insert(index, value) {
+    if(index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const firstPointer = this.getTheIndex(index - 1);
+    const holdingPointer = firstPointer.next;
+    firstPointer.next = newNode;
+    newNode.next = holdingPointer;
+  }
+
+  getTheIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 
@@ -53,3 +81,4 @@ console.log(singly.append(3));
 console.log(singly.append(4));
 console.log(singly.append(5));
 console.log(singly.append(6));
+console.log(singly.prepend(0));
